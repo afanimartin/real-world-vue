7a3106861c04579207e501e24b09760477fa588d
Martin Afani <afani.dev@gmail.com>
2021-12-18 02:20:33 +0200

    Add API service for fetching events

---
+++ src/services/EventService.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const APIClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/afanimartin/real-world-vue',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvents() {
+    return APIClient.get('/events')
+  }
+}
